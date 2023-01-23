7547962a2081cbe324bf1b122142cacddd8a1270
Anonymous <leetong.lam@icloud.com>
2023-01-23 12:55:48 +0800

    added special events easter egg

--- blog_website/app.py
+++ blog_website/app.py
@@ -1,12 +1,23 @@
 import jinja2
 from flask import Flask, render_template, request, url_for
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
+    now = datetime.datetime.now()
+    x = ""
+    if now.month == 1 and now.day == 1:
+        x = "Happy New Year!"
+    elif now.month == 12 and now.day == 25:
+        x = "Merry Christmas!"
+    elif now.month == 10 and now.day == 31:
+        x = "Boo!"
+    else:
+        x = ""
+    return render_template('home.html', sp_event=x)
+      
 @app.route('/about')
 def about():
     return render_template('about.html')
